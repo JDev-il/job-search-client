cfcc37f4398ab1dc191e44d33391e1300cdbf3ac
JDev-il <jonathandanielwork@gmail.com>
2026-03-04 16:40:47 +0200

    Fix(api, data, resolver): fix DTO typings, api response, resolver on init payload

--- src/app/shared/services/resolver.service.ts
+++ src/app/shared/services/resolver.service.ts
@@ -1,25 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Resolve } from '@angular/router';
-import { catchError, Observable, tap, throwError } from 'rxjs';
-import { AuthUserResponse, UserLogin } from '../../core/models/users.interface';
+import { catchError, EMPTY, Observable, tap } from 'rxjs';
+import { AuthorizedUser, AuthUserResponse } from '../../core/models/users.interface';
 import { AuthService } from '../../core/services/auth.service';
 import { DataService } from './data.service';
 import { RoutingService } from './routing.service';
 
 @Injectable({ providedIn: 'root' })
-export class AuthResolver implements Resolve<UserLogin | null> {
+export class AuthResolver implements Resolve<AuthorizedUser | null> {
   constructor(
     private dataService: DataService,
     private authService: AuthService,
     private routingService: RoutingService
   ) { }
 
-  resolve(): Observable<UserLogin | null> {
+  resolve(): Observable<AuthorizedUser | null> {
     return this.dataService.verifyUserToken().pipe(
-      tap((user: UserLogin) => {
+      tap((user: AuthorizedUser) => {
         const userRes = {
-          email: user.email,
-          password: user.password
+          email: user.email
         } as AuthUserResponse;
         this.dataService.usersResponseState = userRes;
       }),
@@ -32,6 +31,6 @@ export class AuthResolver implements Resolve<UserLogin | null> {
   private onError(): Observable<null> {
     this.authService.logout();
     this.routingService.toLogin();
-    return throwError(() => null);
+    return EMPTY;
   }
 }
